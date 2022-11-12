f5fe5c4f502647f9ddf248d9fff07b5a7ece7d67
Ermin Jamaković <ejamakovic1@etf.unsa.ba>
2022-11-12 20:41:40 +0100

    Promjena metode dajMandatskeKandidate.

--- Zadaca1/Izbori.cs
+++ Zadaca1/Izbori.cs
@@ -97,7 +97,10 @@ namespace Zadaca1
                 if ((double)k.BrojGlasova / (double)s.BrojGlasova > 0.2)
         {
                     kandidati1.Add(k);
-                    break;
+                    /*Korištenje break naredbe u ovom slučaju je pogrešno,
+                     obzirom da će na taj način biti dodat samo prvi 
+                     kandidat koji ispunjava uslov*/ 
+
                 }
             }
             return kandidati1;
