4a663c21138513e231db76715b357e5b5a260f4a
nbecirspah1 <nbecirspah1@etf.unsa.ba>
2022-11-30 16:21:20 +0100

    Urađena prva funkcionalnost

--- Zadaca1/Glasac.cs
+++ Zadaca1/Glasac.cs
@@ -72,6 +72,41 @@ namespace Zadaca1
             }
             nezavisniKandidat.povecajBrojGlasova();
         }
+
+        public void ValidacijaPodataka(string ime, string prezime, string adresaStanovanja, DateTime datumRodjenja, string brojLicneKarte, string JMBG){
+            Regex regex = new Regex(@"^[-a-zA-Z]");
+            Regex licnaKarta = new Regex(@"[0-9][0-9][0-9][EJKMT][0-9][0-9][0-9]");
+            var trenutnaGodina = DateTime.Now.ToString("yyyy").Int32.TryParse();
+
+            if(ime == "" || prezime == "" || adresaStanovanja == ""){ 
+                throw new Exception("Niste unijeli ime, prezime ili adresu stanovanja!");
+            }
+            else if(regex.IsMatch(ime) && regex.IsMatch(prezime)){
+                throw new Exception("Ime i prezime smiju sadržavati samo slova i crticu!");
+            }
+            else if(ime.Length < 2 || ime.Length > 40){
+                throw new Exception("Ime se sastoji od minimalno 2, a maksimalno 40 slova!");
+            }
+            else if(prezime.Length < 3 || prezime.Length > 50){
+                throw new Exception("Prezime se sastoji od minimalno 3, a maksimalno 50 slova!");
+            }
+            else if(datumRodjenja < DateTime.Now){
+                throw new Exception("Datum rođenja je u budućnosti!");
+            }
+            else if(trenutnaGodina - datumRodjenja.ToString("yyyy").Int32.TryParse() < 18){
+                throw new Exception("Glasač mora biti punoljetan!");
+            }
+            else if(!licnaKarta.IsMatch(brojLicneKarte) || brojLicneKarte.Length != 7){ 
+                throw new Exception("Pogrešan unos broja lične karte!");   
+            }
+            else if(JMBG.Length != 13 || string.Compare(JMBG.Substring(0,2), datumRodjenja.ToString("dd") != 0
+                    || string.Compare(JMBG.Substring(2,4), datumRodjenja.ToString("MM") != 0
+                    || string.Compare(JMBG.Substring(4,7), datumRodjenja.ToString("yyy") != 0){
+                throw new Exception("JMBG nije validan!");   
+            }
+        }
+
+
         #endregion
     }
 }
--- Zadaca1/Kandidat.cs
+++ Zadaca1/Kandidat.cs
@@ -8,42 +8,46 @@ namespace Zadaca1
 {
     public class Kandidat
     {
+        #region Atributi
         string ime;
         string prezime;
         string informacije;
         int brojGlasova;
         Stranka stranka = null;
+        #endregion
 
-        public Kandidat
-            (string ime, string prezime, Stranka stranka = null)
-        {
-      this.ime = ime;
-        this.prezime = prezime;
-     brojGlasova = 0;      informacije = "";
+        #region Konstruktor
+        public Kandidat (string ime, string prezime, Stranka stranka = null){
+            this.ime = ime;
+            this.prezime = prezime;
+            brojGlasova = 0;
+            informacije = "";
             this.stranka = stranka;
         }
+        #endregion
 
+        #region Properties
         public string Informacije { set => informacije = value; get => informacije;}
         public int BrojGlasova { set => brojGlasova=value; get => brojGlasova; }
         public string NazivKandidata { get => ime + " " + prezime; }
         public string Ime { set => ime=value; get => ime; }
         public string Prezime { set => prezime=value; get => prezime; }
         public Stranka Stranka { set => stranka=value; get => stranka; }
-        
+        #endregion
 
-        public override bool Equals(object obj)
-        {
+        #region Metode
+        public override bool Equals(object obj){
             return obj is Kandidat kandidat &&
                                              ime == kandidat.ime &&
                    prezime == kandidat.prezime &&
                                                   informacije == kandidat.informacije &&
                    brojGlasova == kandidat.brojGlasova && stranka == kandidat.stranka;
-}
+        }
 
 
          public void povecajBrojGlasova() { brojGlasova++; }
 
         public override int GetHashCode(){ return HashCode.Combine(ime,prezime,informacije,brojGlasova,stranka);  }
-        
+        #endregion
     }
 }
